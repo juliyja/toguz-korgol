42e573ed046c0db5b9bc4b7200acbfb85c77877b
Ido <ben.zvi.ido@gmail.com>
2018-11-30 16:53:08 +0000

    Update instructions popup window

--- src/main/java/toguzKorgool/MainWindow.java
+++ src/main/java/toguzKorgool/MainWindow.java
@@ -116,8 +116,9 @@ public class MainWindow {
     private static void setUpInstructions(){
         JOptionPane.showMessageDialog(frame,
                 "Move\n" +
-                "Players make moves by selecting one of the holes on their side of the game that contains korgools. A move consists of taking all the korgools from the selected hole and redistributing or seeding them to other holes in the anticlockwise direction.\n"
-                + "The first korgool is put in the hole the korgools were taken from. The next korgool goes in the adjacent hole to the right, and so on. Once the player has dropped a korgool in hole 9, the next korgool goes into hole 1 of the other player, and so on.\n\n"
+                "Players make moves by selecting one of the holes on their side of the game that contains korgools. A move consists of taking all the korgools from the selected hole\n"
+                + " and redistributing or seeding them to other holes in the anticlockwise direction. The first korgool is put in the hole the korgools were taken from. The next korgool\n"
+                + "goes in the adjacent hole to the right, and so on. Once the player has dropped a korgool in hole 9, the next korgool goes into hole 1 of the other player, and so on.\n\n"
 
                         +"Rules\n" +
                 "1. The player on the white/light side makes the first move. Players alternate between making moves.\n"
