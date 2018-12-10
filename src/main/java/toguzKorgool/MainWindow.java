1df430f0b65713ee2912de86bc9cfecaea006abc
Ido <ben.zvi.ido@gmail.com>
2018-12-10 01:22:05 +0000

    Refactor code and link to board class

--- src/main/java/toguzKorgool/MainWindow.java
+++ src/main/java/toguzKorgool/MainWindow.java
@@ -1,16 +1,13 @@
 package toguzKorgool;
 
 import javafx.embed.swing.JFXPanel;
-import java.io.IOException;
+
 import java.awt.*;
-import java.awt.event.*;
 import java.util.concurrent.CountDownLatch;
 import javax.swing.*;
 
 /**
- * The application's main window for the user. Contains three main buttons: Single Player, Load Game
- * and Instructions. Single Player starts a new game. Load Game resumes a previously saved game.
- * Instructions displays a pop-up window with instructions for how to play the game.
+ * The application's main window for the user.
  *
  * @author Ido Ben-zvi
  * @version 15/11/2018
@@ -18,35 +15,26 @@ import javax.swing.*;
 public class MainWindow {
 
     private static JFrame frame;
-    private static JButton singlePlayer;
-    private static JButton loadGame;
-    private static JButton instructions;
 
     public static void main(String[] args)throws InterruptedException{
-        MainWindow window = new MainWindow();
+        new MainWindow();
     }
 
     /**
      * Set up main window.
      */
-    public MainWindow() throws InterruptedException
+    private MainWindow() throws InterruptedException
     {
         final CountDownLatch latch = new CountDownLatch(1);
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new JFXPanel();
-                latch.countDown();
-            }
+        SwingUtilities.invokeLater(() -> {
+            new JFXPanel();
+            latch.countDown();
         });
-            latch.await();
+        latch.await();
 
         frame = new JFrame("Main Menu");
         setUpMainWindow();
         setUpMenu();
-
-        singlePlayer.setName("singlePlayer");
-        loadGame.setName("loadGame");
-        instructions.setName("instructions");
     }
 
     /**
@@ -54,7 +42,10 @@ public class MainWindow {
      */
     private static void setUpMainWindow()
     {
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        //frame.setLayout(new GridLayout(4, 1));
+
+
         frame.setLocationRelativeTo(null);
         frame.pack();
         frame.setVisible(true);
@@ -62,45 +53,28 @@ public class MainWindow {
     }
 
     private static void setUpMenu(){
-        singlePlayer = new JButton("Single Player");
+        JButton singlePlayer = new JButton("Single Player");
         singlePlayer.setPreferredSize(new Dimension(300, 150));
-        singlePlayer.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    FileEditor editor = new FileEditor(true);
-                    Player_Board.launch();
-                } catch (IOException e1) {
-                    System.out.println("ERROR");
-                }
-            }
+
+        singlePlayer.addActionListener(e -> {
+            new FileEditor(true, "");
+            Player_Board.launch();
         });
 
-        loadGame = new JButton ("Load Game");
+        JButton loadGame = new JButton ("Load Game");
         loadGame.setPreferredSize(new Dimension(300, 150));
-        loadGame.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                try {
-                    FileEditor editor = new FileEditor(false);
-                    Player_Board.launch();
-                } catch (IOException e1) {
-                    System.out.println("ERROR");
-                }
-            }
+
+        loadGame.addActionListener(e -> {
+            new FileEditor(false, "save");
+            Player_Board.launch();
         });
 
-        instructions  = new JButton ("Intructions");
-        instructions.setPreferredSize(new Dimension(300,150));
-        instructions.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setUpInstructions();
-            }
-        });
+        JButton instructions  = new JButton ("Intructions");
+        instructions.addActionListener(e -> setUpInstructions());
 
         Container pane = frame.getContentPane();
         pane.setLayout(new BoxLayout(pane, BoxLayout.Y_AXIS));
+
         singlePlayer.setAlignmentX(Component.CENTER_ALIGNMENT);
         pane.add(singlePlayer);
         loadGame.setAlignmentX(Component.CENTER_ALIGNMENT);
@@ -112,27 +86,27 @@ public class MainWindow {
     private static void setUpInstructions(){
         JOptionPane.showMessageDialog(frame,
                 "Move\n" +
-                "Players make moves by selecting one of the holes on their side of the game that contains korgools. \n" +
-                "A move consists of taking all the korgools from the selected hole\n"
-                + " and redistributing or seeding them to other holes in the anticlockwise direction. The first korgool is put in the hole the korgools were taken from. The next korgool\n"
-                + "goes in the adjacent hole to the right, and so on. Once the player has dropped a korgool in hole 9, the next korgool goes into hole 1 of the other player, and so on.\n\n"
+                        "Players make moves by selecting one of the holes on their side of the game that contains korgools. \n" +
+                        "A move consists of taking all the korgools from the selected hole\n"
+                        + " and redistributing or seeding them to other holes in the anticlockwise direction. The first korgool is put in the hole the korgools were taken from. The next korgool\n"
+                        + "goes in the adjacent hole to the right, and so on. Once the player has dropped a korgool in hole 9, the next korgool goes into hole 1 of the other player, and so on.\n\n"
 
                         +"Rules\n" +
-                "1. The player on the white/light side makes the first move. Players alternate between making moves.\n"
-                + "2.The normal redistribution rule does not apply if there is only one korgool in a hole. If there is only one korgool \n" +
-                   "in a hole and that hole is selected for a move, the hole is emptied and the korgool is moved to the next hole.\n"
-                + "3. If the last korgool in a move ends up in an opponent's hole, and the number of korgools in that hole is now even, then the player \n" +
+                        "1. The player on the white/light side makes the first move. Players alternate between making moves.\n"
+                        + "2.The normal redistribution rule does not apply if there is only one korgool in a hole. If there is only one korgool \n" +
+                        "in a hole and that hole is selected for a move, the hole is emptied and the korgool is moved to the next hole.\n"
+                        + "3. If the last korgool in a move ends up in an opponent's hole, and the number of korgools in that hole is now even, then the player \n" +
                         "captures all the korgools in that hole. The korgools are moved into the player's kazan.\n"
-                + "4. If the last korgool in a move ends up in the player's own side, nobody captures these korgools.\n"
-                + "5. If the last korgool in a move ends up in an opponent's hole, and the number of korgools in that hole is now odd, then nobody captures these korgools.\n"
-                + "6. If the last korgool in a move ends up in an opponent's hole containing two korgools (i.e. the hole contains three korgools at the end of the move), \n" +
+                        + "4. If the last korgool in a move ends up in the player's own side, nobody captures these korgools.\n"
+                        + "5. If the last korgool in a move ends up in an opponent's hole, and the number of korgools in that hole is now odd, then nobody captures these korgools.\n"
+                        + "6. If the last korgool in a move ends up in an opponent's hole containing two korgools (i.e. the hole contains three korgools at the end of the move), \n" +
                         "then this hole is marked as a tuz.\n"
-                +"   This means that this hole now belongs to the player who claimed the tuz and all korgools in the tuz are transferred to the owner's kazan. \n" +
+                        +"   This means that this hole now belongs to the player who claimed the tuz and all korgools in the tuz are transferred to the owner's kazan. \n" +
                         "The following restrictions apply:\n"
-                + "  - Each player can claim only one tuz.\n"
-                + "  - A tuz cannot be moved.\n"
-                + "  - Hole 9 cannot be claimed as tuz.\n"
-                + "  - If one player has claimed hole n as tuz, then their opponent can no longer claim hole n on the opposite side as tuz.\n"
-                + "7. The game ends when one player has collected 82 or more korgools in their kazan.");
+                        + "  - Each player can claim only one tuz.\n"
+                        + "  - A tuz cannot be moved.\n"
+                        + "  - Hole 9 cannot be claimed as tuz.\n"
+                        + "  - If one player has claimed hole n as tuz, then their opponent can no longer claim hole n on the opposite side as tuz.\n"
+                        + "7. The game ends when one player has collected 82 or more korgools in their kazan.");
     }
 }
