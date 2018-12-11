6f0a76e88ed8ff8064e1c12fe6d9193abda2e26a
Kirill Zasimov <kirillzasimov@gmail.com>
2018-12-11 14:47:42 +0000

    Add AIPlayer class. Add tests for GameState functionality.

--- src/test/java/toguzKorgool/GameLogicTest.java
+++ src/test/java/toguzKorgool/GameLogicTest.java
@@ -1,8 +1,14 @@
 package toguzKorgool;
 
-import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class GameLogicTest {
+    @Test
+    public void settingStateToRunning(){
+        GameLogic instance = GameLogic.getInstance();
+        instance.setStateToRunning();
+        assertEquals(instance.getState(), GameState.RUNNING);
+    }
 
 }
